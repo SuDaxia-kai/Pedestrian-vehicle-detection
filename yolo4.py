33b761c3dce845d8d69323eaa041d61d8a2759a2
SuDaxia-kai <2936200754@qq.com>
2022-08-07 22:35:58 +0800

    完成了yolobody的编写

--- yolo4.py
+++ yolo4.py
@@ -1,17 +1,17 @@
-from turtle import forward
 import torch
 import torch.nn as nn
 from collections import OrderedDict
-from CSPDarkenet import darknet53
+from torch.autograd import Variable
+from CSPDarkenet import *
 
 # CBL的构建
 def conv2d(filter_in, filter_out, kernel_size, stride = 1):
     pad = (kernel_size - 1) // 2 if kernel_size else 0    # 其实此处应该是可以用(kernel_size // 2来代替)
-    return nn.Sequential(OrderedDict[
-        ('conv', nn.Conv2d(filter_in, filter_out, kernel_size = kernel_size, stride = stride, padding = pad)),
+    return nn.Sequential(OrderedDict([
+        ('conv', nn.Conv2d(filter_in, filter_out, kernel_size = kernel_size, stride = stride, padding = pad, bias = False)),
         ('bn', nn.BatchNorm2d(filter_out)),
         ('relu', nn.LeakyReLU(0.1))
-    ])
+    ]))
     
 
 #--------------------------------------------------#
@@ -85,3 +85,77 @@ def yolo_head(filters_list, in_filters):
     )
     return m
 
+#---------------------------------------------------#
+#   yolov4_body
+#---------------------------------------------------#
+class YoloBody(nn.Module):
+    def __init__(self, num_anchors, num_classes):
+        super(YoloBody, self).__init__()
+        #backbone
+        self.backbone = darknet53(None)
+        
+        # neck_model1
+        self.conv1 = make_three_conv([512, 1024], 1024)
+        self.SPP = SpatiaPvramidPooling()
+        self.conv2 = make_three_conv([512, 1024, 512], 2048)
+    
+        # neck_model2
+        self.upsample1 = Upsample(512, 256)   # 256 -> 256吧
+        self.conv_for_P4 = conv2d(512, 256, 1)
+        self.make_five_conv1 = make_five_conv([256, 512], 512)
+        
+        # neck_model3
+        self.upsample2 = Upsample(256, 128)  # 128 -> 128吧
+        self.conv_for_P3 = conv2d(256, 128, 1)
+        self.make_five_conv2 = make_five_conv([128, 256], 256)
+        final_out_filter2 = num_anchors * (5 + num_classes)
+        self.yolo_head3 = yolo_head([256, final_out_filter2], 128)
+        
+        # neck_model4
+        self.down_sample1 = conv2d(128, 256, 3, stride = 2)
+        self.make_five_conv3 = make_five_conv([256, 512], 512)
+        final_out_filter1 = num_anchors * (5 + num_classes)
+        self.yolo_head2 = yolo_head([512, final_out_filter1], 256)
+        
+        # neck_model5
+        self.down_sample2 = conv2d(256, 512, 3, stride = 2)
+        self.make_five_conv4 = make_five_conv([512, 1024], 1024)
+        final_out_filter0 = num_anchors * (5 + num_classes)
+        self.yolo_head1 = yolo_head([1024, final_out_filter0], 512)
+        
+    def forward(self, x):
+        #  backbone
+        x2, x1, x0 = self.backbone(x)
+
+        P5 = self.conv1(x0)
+        P5 = self.SPP(P5)
+        P5 = self.conv2(P5)
+
+        P5_upsample = self.upsample1(P5)
+        P4 = self.conv_for_P4(x1)
+        P4 = torch.cat([P4,P5_upsample],axis=1)
+        P4 = self.make_five_conv1(P4)
+
+        P4_upsample = self.upsample2(P4)
+        P3 = self.conv_for_P3(x2)
+        P3 = torch.cat([P3,P4_upsample],axis=1)
+        P3 = self.make_five_conv2(P3)
+
+        P3_downsample = self.down_sample1(P3)
+        P4 = torch.cat([P3_downsample,P4],axis=1)
+        P4 = self.make_five_conv3(P4)
+
+        P4_downsample = self.down_sample2(P4)
+        P5 = torch.cat([P4_downsample,P5],axis=1)
+        P5 = self.make_five_conv4(P5)
+
+        out2 = self.yolo_head3(P3)
+        out1 = self.yolo_head2(P4)
+        out0 = self.yolo_head1(P5)
+
+        return out0, out1, out2
+
+
+if __name__ == '__main__':
+    model = YoloBody(3, 80)
+    load_model_pth(model,"pth/yolo4_weights.pth")
